2174d4b251dfe8ab5d16e730b2681384611c8084
yoann robin <yoann.robin.k@gmail.com>
2019-06-24 06:35:35 +0200

    Good version number

--- python/setup.py
+++ python/setup.py
@@ -22,7 +22,7 @@ list_packages = [
 
 setup(
 	name = "NSSEA" ,
-	version = "0.2.5a1" ,
+	version = "0.2.4" ,
 	description = "",
 	author = "Yoann Robin" ,
 	author_email = "yoann.robin.k@gmail.com" ,
