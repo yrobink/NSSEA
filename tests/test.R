2dee83d0bd1eafd17faef12e7241f1b4103025fd
ROBIN Yoann <yoann.robin.k@gmail.com>
2019-07-09 15:09:47 +0200

    ---

--- tests/test.R
+++ tests/test.R
@@ -46,14 +46,14 @@ library(abind)
 ##########
 
 ## Test or not
-is_test = TRUE
+is_test = FALSE
 if( is_test )
 	base::set.seed(42) ## Always the big answer when we set the seed
 
 
 ## Path
 pathInput = "data"
-pathOut   = "/home/robiny/Local/scratch"
+pathOut   = "/home/robiny/Local/scratch/NSSEA_R"
 #pathOutput = "/home/yrobin/Local/scratch/EUPHEME/R"
 
 ## Command line arguments
@@ -63,7 +63,7 @@ code      = "yrobin"
 ## Global parameters
 n_sample    = if( is_test ) 10 else 100
 ns_law      = NSSEA::NSGaussianModel
-ns_law_args = NULL #list( use_phi = FALSE )
+ns_law_args = list( link_fct_scale = SDFC::IdLinkFct$new() )
 gam_dof     = 7
 verbose     = TRUE
 time        = 1850:2100
@@ -85,6 +85,8 @@ Y = Y_full
 models   = names(X_full)
 n_models = length(models)
 
+## In anomaly
+
 ## Load observations
 base::load( base::file.path( pathInput , "Xo.Rdata" ) )
 base::load( base::file.path( pathInput , "Yo.Rdata" ) )
@@ -101,99 +103,75 @@ for( i in 1:n_models )
 
 
 ## Define coffee variable from input
-coffee = make_coffee( time , n_sample , models , ns_law , ns_law_args )
+coffee = NSSEA::make_coffee( time , n_sample , models , ns_law , ns_law_args )
 
 
 ### Split
 Enat = NSSEA::ebm_response( coffee$time , coffee$n_sample )
+X_splitted = NSSEA::gam_decomposition( X_full , Enat , event$time , gam_dof , verbose = verbose , code = code )
+coffee$X = if( is.null(X_splitted$X_center) ) X_splitted$X else X_splitted$X_center
 
-dof = 7
-		x      = X_full[[i]]
-		time_x = as.numeric(names(x))
-		idx_Xl = match( time_x , as.numeric(names(Enat[,1])) )
-		Xl = Enat[idx_Xl,1]
-		
-		## Best estimate
-		tol = 1e-2
-		edof = dof + 1. + tol
-		rup = 1e2
-		rlo = 1e-2
-		while( abs( dof - edof ) > tol )
-		{
-			r = (rup + rlo) / 2
-			print(r)
-			gam_model = mgcv::gam( X ~ s( t , k = dof + 2 , fx = FALSE , sp = r ) + Xl , data = data.frame( X = x , Xl = Xl , t = time_x ) )
-			edof = sum( gam_model$edf )
-			if( edof < dof )
-				rup = r
-			else
-				rlo = r
-		}
 
-#X_splitted = NSSEA::gam_decomposition( X_full , Enat , Sigma , event$time , gam_dof , verbose = verbose , code = code )
-#coffee$X = if( is.null(X_splitted$X_center) ) X_splitted$X else X_splitted$X_center
-#
-#
-### NS fit
-#coffee = NSSEA::nslaw_fit( Y , coffee , verbose = verbose , code = code )
-#
-#
+## NS fit
+coffee = NSSEA::nslaw_fit( Y , coffee , verbose = verbose , code = code )
+
+
 ### Multi model
-#coffee = NSSEA::infer_multi_model( coffee , mm_method , verbose )
-#
-#
-### Apply constraints
-##coffeeCX   = NSSEA::constraints_CX( coffee   , Xo , cx_params , Sigma , verbose = verbose )
-##coffeeC0   = NSSEA::constraints_C0( coffee   , Yo , event , verbose = verbose )
-##coffeeCXC0 = NSSEA::constraints_C0( coffeeCX , Yo , event , verbose = verbose )
-#
-#
+coffee = NSSEA::infer_multi_model( coffee , verbose = verbose )
+
+
+## Apply constraints
+coffeeCX   = NSSEA::constraints_CX( coffee   , Xo , cx_params , verbose = verbose )
+coffeeC0   = NSSEA::constraints_C0( coffee   , Yo , event , verbose = verbose )
+coffeeCXC0 = NSSEA::constraints_C0( coffeeCX , Yo , event , verbose = verbose )
+
+
 ### Compute stats
-#coffee     = NSSEA::extremes_stats( coffee     , event , verbose = verbose , code = code )
-##coffeeCX   = NSSEA::extremes_stats( coffeeCX   , event , verbose = verbose , code = code )
-##coffeeC0   = NSSEA::extremes_stats( coffeeC0   , event , verbose = verbose , code = code )
-##coffeeCXC0 = NSSEA::extremes_stats( coffeeCXC0 , event , verbose = verbose , code = code )
-#
+coffee     = NSSEA::extremes_stats( coffee     , event , verbose = verbose , code = code )
+coffeeCX   = NSSEA::extremes_stats( coffeeCX   , event , verbose = verbose , code = code )
+coffeeC0   = NSSEA::extremes_stats( coffeeC0   , event , verbose = verbose , code = code )
+coffeeCXC0 = NSSEA::extremes_stats( coffeeCXC0 , event , verbose = verbose , code = code )
+
 #if( FALSE )
 #{
-### Plot 
-#NSSEA::plot_X_dec( X_full , X_splitted$X ,  ofile = base::file.path( event$dir_name , "Covariate_decomposition.pdf" ) , verbose = verbose )	# X-decomposition
-#NSSEA::plot_ns_params_fitted( Y , coffee ,  ofile = base::file.path( event$dir_name , "ns_params_fitted.pdf"        ) , verbose = verbose )	# NS-fit
-#NSSEA::plot_stats_probability( coffee ,     ofile = base::file.path( event$dir_name , "Stats_probability.pdf"       ) , verbose = verbose )
-#NSSEA::plot_stats_intensity( coffee ,       ofile = base::file.path( event$dir_name , "Stats_intensity.pdf"         ) , verbose = verbose )
-#NSSEA::plot_relative( coffee , event$time , ofile = base::file.path( event$dir_name , "Stats_relative.pdf"          ) , verbose = verbose )
+## Plot 
+NSSEA::plot_X_dec( X_full , X_splitted$X ,  ofile = base::file.path( event$dir_name , "Covariate_decomposition.pdf" ) , verbose = verbose )	# X-decomposition
+NSSEA::plot_ns_params_fitted( Y , coffee ,  ofile = base::file.path( event$dir_name , "ns_params_fitted.pdf"        ) , verbose = verbose )	# NS-fit
+NSSEA::plot_stats_probability( coffee ,     ofile = base::file.path( event$dir_name , "Stats_probability.pdf"       ) , verbose = verbose )
+NSSEA::plot_stats_intensity( coffee ,       ofile = base::file.path( event$dir_name , "Stats_intensity.pdf"         ) , verbose = verbose )
+NSSEA::plot_relative( coffee , event$time , ofile = base::file.path( event$dir_name , "Stats_relative.pdf"          ) , verbose = verbose )
+
+
+## Multi-model
+#NSSEA::plot_multimodel( coffee , ofile = base::file.path( event$dir_name , "Multi-model.pdf" ) ) # Multi-model
+
+## Constraints
+NSSEA::plot_constraints_CX( coffee , coffeeCX , Xo , ref_plot = event$ref_anom , ofile = base::file.path( event$dir_name , "CX.pdf" ) , verbose = verbose )
+NSSEA::plot_stats_probability( coffeeCXC0 ,                                      ofile = base::file.path( event$dir_name , "Stats_probability_CXC0.pdf" ) , verbose = verbose )
+NSSEA::plot_stats_intensity( coffeeCXC0 ,                                        ofile = base::file.path( event$dir_name , "Stats_intensity_CXC0.pdf"   ) , verbose = verbose )
+
+
+## Stats relative to the event
+#NSSEA::plot_stats_time( coffee$stats     , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,".pdf"     ) ) , verbose = verbose )
+#NSSEA::plot_stats_time( coffeeCX$stats   , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_CX.pdf"  ) ) , verbose = verbose )
+#NSSEA::plot_stats_time( coffeeC0$stats   , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_C0.pdf"  ) ) , verbose = verbose )
+#NSSEA::plot_stats_time( coffeeCXC0$stats , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_CXC0.pdf") ) , verbose = verbose )
 #
 #
-### Multi-model
-##NSSEA::plot_multimodel( coffee , ofile = base::file.path( event$dir_name , "Multi-model.pdf" ) ) # Multi-model
+#stats_tmp = abind::abind( coffee$stats[,,,"multi"],
+#						coffeeCX$stats[,,,"multi"],
+#						coffeeC0$stats[,,,"multi"],
+#						coffeeCXC0$stats[,,,"multi"],
+#						along = 4,
+#						use.dnns = TRUE,
+#						new.names = base::c( dimnames(coffee$stats)[1:3] , list( models = base::c("No obs","CX","C0","CX+C0") ) )
+#						)
 #
-### Constraints
-#NSSEA::plot_constraints_CX( coffee , coffeeCX , Xo , ref_plot = event$ref_anom , ofile = base::file.path( event$dir_name , "CX.pdf" ) , verbose = verbose )
-#NSSEA::plot_stats_probability( coffeeCXC0 ,                                      ofile = base::file.path( event$dir_name , "Stats_probability_CXC0.pdf" ) , verbose = verbose )
-#NSSEA::plot_stats_intensity( coffeeCXC0 ,                                        ofile = base::file.path( event$dir_name , "Stats_intensity_CXC0.pdf"   ) , verbose = verbose )
-#
-#
-### Stats relative to the event
-##NSSEA::plot_stats_time( coffee$stats     , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,".pdf"     ) ) , verbose = verbose )
-##NSSEA::plot_stats_time( coffeeCX$stats   , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_CX.pdf"  ) ) , verbose = verbose )
-##NSSEA::plot_stats_time( coffeeC0$stats   , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_C0.pdf"  ) ) , verbose = verbose )
-##NSSEA::plot_stats_time( coffeeCXC0$stats , event$time , ofile = base::file.path( event$dir_name , base::paste0("Diags_",event$time,"_CXC0.pdf") ) , verbose = verbose )
-##
-##
-##stats_tmp = abind::abind( coffee$stats[,,,"multi"],
-##						coffeeCX$stats[,,,"multi"],
-##						coffeeC0$stats[,,,"multi"],
-##						coffeeCXC0$stats[,,,"multi"],
-##						along = 4,
-##						use.dnns = TRUE,
-##						new.names = base::c( dimnames(coffee$stats)[1:3] , list( models = base::c("No obs","CX","C0","CX+C0") ) )
-##						)
-##
-##NSSEA::plot_stats_time( stats_tmp , event$time , ofile = base::file.path( event$dir_name , "MultiC.pdf" ) , verbose = verbose )
-#
+#NSSEA::plot_stats_time( stats_tmp , event$time , ofile = base::file.path( event$dir_name , "MultiC.pdf" ) , verbose = verbose )
+
 #}
-#
-### Save
+
+## Save
 ##base::save( list = base::c( "event" , "mm_method" , "code" , "Enat" , "coffee" , "coffeeCX" , "coffeeC0" , "coffeeCXC0" , "X_splitted" ) , file = base::file.path( event$dir_name , "Results.Rdata" ) )
 #
 #coffee2netcdf( coffee , event , ofile = base::file.path( event$dir_name , "Coffee.nc" ) )
