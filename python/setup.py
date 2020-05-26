24d12abeff7fadd3ceb41febbf994bde204d04a7
yrobink <yoann.robin.k@gmail.com>
2020-05-26 09:04:15 +0200

    version 0.3.0a15

--- python/setup.py
+++ python/setup.py
@@ -22,7 +22,7 @@ list_packages = [
 
 setup(
 	name = "NSSEA" ,
-	version = "0.3.0a14" ,
+	version = "0.3.0a15" ,
 	description = "",
 	author = "Yoann Robin" ,
 	author_email = "yoann.robin.k@gmail.com" ,
