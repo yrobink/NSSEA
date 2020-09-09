c41718566558bddda35067a136ada9b73f94af71
Yoann Robin <yoann.robin.k@gmail.com>
2020-09-09 17:12:02 +0200

    GAM decomposition plot, add synthesis

--- python/NSSEA/plot/__covariates.py
+++ python/NSSEA/plot/__covariates.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 ##################################################################################
 ##################################################################################
@@ -85,26 +84,163 @@
 ##################################################################################
 ##################################################################################
 
-#############
-## Imports ##
-#############
+###############
+## Libraries ##
+###############
 
-from .__linkParams       import LinkParams
-from .__decomposition    import decomposition
-from .__probabilities    import probabilities
-from .__probabilities    import probabilities_not_zero
-from .__probabilities    import return_time
-from .__intensities      import intensities
-from .__stats_event      import stats_event
-from .__stats_event      import stats_relative
-from .__constraintsCX    import constraints_CX
-from .__ns_params        import ns_params
-from .__ns_params        import ns_params_comparison
-from .__ns_params        import ns_params_time
-from .__classic_packages import plot_classic_packages
+import numpy  as np
+import pandas as pd
+import xarray as xr
 
-from .__print_stats      import summary_table
-from .__print_stats      import summary_event
+import matplotlib as mpl
+#mpl.use("pdf")
+import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf as mpdf
 
-from .__covariates       import GAM_decomposition
+from NSSEA.__tools import ProgressBar
+
+
+###############
+## Functions ##
+###############
+
+def GAM_decomposition( clim , lX , event , ofile , ci = 0.05 , verbose = False ): ##{{{
+	"""
+	NSSEA.plot.GAM_decomposition
+	============================
+	
+	Plot the GAM decomposition of covariates
+	
+	Arguments
+	---------
+	Xd        : pandas.DataFrame
+		Covariates before decomposition
+	X         : xarray
+		Covariates afer decomposition (NSSEA.Climatology.X)
+	event     : NSSEA.Event
+		Event variable
+	ofile     : str
+		output file
+	ci        : float
+		Size of confidence interval, default is 0.05 (95% confidence)
+	verbose   : bool
+		Print (or not) state of execution
+	"""
+	
+	pb = ProgressBar( clim.n_model , "GAM_decomposition" , verbose )
+	
+	X = clim.X.copy()
+	XA = ( X.loc[:,:,"F",:] - X.loc[:,:,"C",:] ).assign_coords( forcing = "A" )
+	X = xr.concat( [X,XA] , dim = "forcing" )
+	
+	Xl = X[:,1:,:,:].quantile( ci / 2.      , dim = "sample" )
+	Xu = X[:,1:,:,:].quantile( 1. - ci / 2. , dim = "sample" )
+	
+	pdf = mpdf.PdfPages( ofile )
+	
+	## Remove multi
+	models = clim.model
+	if "Multi_Synthesis" in models:
+		models = models[:-1]
+	
+	ymin = min( float(X.loc[:,:,["F","C"],:].min()) , min([ float(lx.min()) for lx in lX]) )
+	ymax = max( float(X.loc[:,:,["F","C"],:].max()) , max([ float(lx.max()) for lx in lX]) )
+	
+	yminAnt = float(X.loc[:,:,"A",:].min())
+	ymaxAnt = float(X.loc[:,:,"A",:].max())
+	
+	ylabel = r"${}$".format( "\mathrm{" + event.name_variable +"}\ \mathrm{(" + event.unit_variable + ")}" )
+	
+	## Loop
+	for i,m in enumerate(models):
+		pb.print()
+		nrow,ncol = 3,1
+		fs = 10
+		fig = plt.figure( figsize = ( fs * ncol , 0.4 * fs * nrow ) )
+		
+		ax = fig.add_subplot( nrow , ncol , 1 )
+		ax.plot( lX[i].index , lX[i].values.ravel() , color = "grey" , linestyle = "" , marker = "." , alpha = 0.3 , label = "" )
+		ax.plot( X.time , X.loc[:,"BE","F",m] , color = "red" , linestyle = "-" , marker = "" , label = r"$\mathrm{Factual}$" )
+		ax.fill_between( X.time , Xl.loc[:,"F",m] , Xu.loc[:,"F",m] , color = "red" , alpha = 0.5 )
+		ax.set_ylim( (ymin,ymax) )
+		ax.legend( loc = "upper left" )
+		ax.set_xticks([])
+		ax.set_title( "{}".format( str(m).replace("_"," ") ) )
+		ax.set_ylabel( ylabel )
+		
+		ax = fig.add_subplot( nrow , ncol , 2 )
+		ax.plot( X.time , X.loc[:,"BE","C",m] , color = "blue" , linestyle = "-" , marker = "" , label = r"$\mathrm{Counter-factual}$" )
+		ax.fill_between( X.time , Xl.loc[:,"C",m] , Xu.loc[:,"C",m] , color = "blue" , alpha = 0.5 )
+		ax.set_ylim( (ymin,ymax) )
+		ax.set_xticks([])
+		ax.legend( loc = "upper left" )
+		ax.set_ylabel( ylabel )
+		
+		ax = fig.add_subplot( nrow , ncol , 3 )
+		ax.plot( X.time , X.loc[:,"BE","A",m] , color = "green" , linestyle = "-" , marker = "" , label = r"$\mathrm{Anthropic}$" )
+		ax.fill_between( X.time , Xl.loc[:,"A",m] , Xu.loc[:,"A",m] , color = "green" , alpha = 0.5 )
+		ax.set_ylim( (yminAnt,ymaxAnt) )
+		ax.legend( loc = "upper left" )
+		ax.set_xlabel( "Time" )
+		ax.set_ylabel( ylabel )
+		
+		xlim = ax.get_xlim()
+		ylim = ax.get_ylim()
+		ax.plot( xlim , [0,0] , color = "black" , linestyle = "-" , marker = "" )
+		ax.set_xlim(xlim)
+		ax.set_ylim(ylim)
+		
+		fig.set_tight_layout(True)
+		pdf.savefig( fig )
+		plt.close(fig)
+	
+	if "Multi_Synthesis" in clim.model:
+		pb.print()
+		nrow,ncol = 3,1
+		fs = 10
+		fig = plt.figure( figsize = ( fs * ncol , 0.4 * fs * nrow ) )
+		m = "Multi_Synthesis"
+		
+		ax = fig.add_subplot( nrow , ncol , 1 )
+		ax.plot( X.time , X.loc[:,"BE","F",m] , color = "red" , linestyle = "-" , marker = "" , label = r"$\mathrm{Factual}$" )
+		ax.fill_between( X.time , Xl.loc[:,"F",m] , Xu.loc[:,"F",m] , color = "red" , alpha = 0.5 )
+		ax.set_ylim( (ymin,ymax) )
+		ax.legend( loc = "upper left" )
+		ax.set_xticks([])
+		ax.set_title( "{}".format( str(m).replace("_"," ") ) )
+		ax.set_ylabel( ylabel )
+		
+		ax = fig.add_subplot( nrow , ncol , 2 )
+		ax.plot( X.time , X.loc[:,"BE","C",m] , color = "blue" , linestyle = "-" , marker = "" , label = r"$\mathrm{Counter-factual}$" )
+		ax.fill_between( X.time , Xl.loc[:,"C",m] , Xu.loc[:,"C",m] , color = "blue" , alpha = 0.5 )
+		ax.set_ylim( (ymin,ymax) )
+		ax.set_xticks([])
+		ax.legend( loc = "upper left" )
+		ax.set_ylabel( ylabel )
+		
+		ax = fig.add_subplot( nrow , ncol , 3 )
+		ax.plot( X.time , X.loc[:,"BE","A",m] , color = "green" , linestyle = "-" , marker = "" , label = r"$\mathrm{Anthropic}$" )
+		ax.fill_between( X.time , Xl.loc[:,"A",m] , Xu.loc[:,"A",m] , color = "green" , alpha = 0.5 )
+		ax.set_ylim( (yminAnt,ymaxAnt) )
+		ax.legend( loc = "upper left" )
+		ax.set_xlabel( "Time" )
+		ax.set_ylabel( ylabel )
+		
+		xlim = ax.get_xlim()
+		ylim = ax.get_ylim()
+		ax.plot( xlim , [0,0] , color = "black" , linestyle = "-" , marker = "" )
+		ax.set_xlim(xlim)
+		ax.set_ylim(ylim)
+		
+		fig.set_tight_layout(True)
+		pdf.savefig( fig )
+		plt.close(fig)
+	
+	pdf.close()
+	
+	pb.end()
+
+##}}}
+
+
 
