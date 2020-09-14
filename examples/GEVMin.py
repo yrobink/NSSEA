3c4df8681d57d84ee0b0d779c871afd93495dcfa
Yoann Robin <yoann.robin.k@gmail.com>
2020-09-14 08:45:29 +0200

    Update GEVMin with new syntax

--- examples/GEVMin.py
+++ examples/GEVMin.py
@@ -91,10 +91,7 @@
 
 import sys,os
 import pickle as pk
-import matplotlib as mpl
-mpl.use("pdf")
-import matplotlib.pyplot as plt
-import matplotlib.backends.backend_pdf as mpdf
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -104,9 +101,6 @@ import SDFC.tools as sdt
 import NSSEA as ns
 import NSSEA.plot as nsp
 import NSSEA.models as nsm
-
-#from NSSEA.plot import LinkParams
-
 
 
 ####################
@@ -209,19 +203,25 @@ def load_models_obs( path ):##{{{
 	Xo  = pd.DataFrame( dXo.temperature_anomaly.values.squeeze() , columns = ["Xo"] , index = np.arange( 1850 , 2019 , 1 , dtype = np.int ) )
 	
 	dYo = xr.open_dataset( os.path.join( path , "Yo.nc" ) )
-	Yo  = pd.DataFrame( dYo.Tm.values.squeeze() , columns = ["Yo"] , index = np.arange( 1947 , 2020 , 1 , dtype = np.int ) )
+	Yo  = pd.DataFrame( dYo.tg.values.squeeze() , columns = ["Yo"] , index = np.arange( 1950 , 2020 , 1 , dtype = np.int ) )
 	
 	return models,lX,lY,Xo,Yo
 ##}}}
 
+class NumpyLog: ##{{{
+	def __init__(self):
+		self._msg = []
+	
+	def __repr__(self):
+		return self.__str__()
+	
+	def __str__(self):
+		return "".join(self._msg)
+	
+	def write( self , msg ):
+		self._msg.append(msg)
+##}}}
 
-####################
-## Plot functions ##
-####################
-
-#############
-## Classes ##
-#############
 
 ##########
 ## main ##
@@ -233,6 +233,13 @@ if __name__ == "__main__":
 	##============
 	is_test = True
 	if is_test: np.random.seed(42) ## A good seed is always the big answer
+	
+	## Set a log class, the GEV produces sometimes overflow
+	##=====================================================
+	nplog = NumpyLog()
+	np.seterrcall(nplog)
+	np.seterr( all = "log" , invalid = "log" )
+	warnings.simplefilter("ignore")
 	
 	## Path
 	##=====
@@ -249,10 +256,10 @@ if __name__ == "__main__":
 	time_reference = np.arange( 1961 , 1991 , 1 , dtype = np.int )
 	n_mcmc_drawn_min = 500  if is_test else  5000
 	n_mcmc_drawn_max = 1000 if is_test else 10000
-	min_rate_accept  = 0.
+	min_rate_accept  = 0.05
 	n_sample    = 1000 if not is_test else 10
 	ns_law      = nsm.GEVMin()
-	event       = ns.Event( "HW19D3_min" , 2019 , None , time_reference , type_event = "hard" , side = "lower" , name_variable = "-TX_3D" , unit_variable = "K" )
+	event       = ns.Event( "HW19D3_min" , 2019 , time_reference , type_ = "hard" , side = "lower" , variable = "minus_TX3D" , unit = "K" )
 	verbose     = True
 	ci          = 0.05 if not is_test else 0.1
 	
@@ -281,13 +288,13 @@ if __name__ == "__main__":
 	
 	## Define clim variable from input
 	##================================
-	clim = ns.Climatology( time_period , models , ns_law )
+	clim = ns.Climatology2( event , time_period , models , n_sample , ns_law )
 	
 	
 	## Decomposition of covariates
 	##============================
 	Xebm   = ns.EBM().draw_sample( clim.time , n_sample + 1 , fix_first = 0 )
-	clim.X = ns.gam_decomposition( lX , Xebm , verbose = verbose )
+	clim   = ns.covariates_FC_GAM( clim , lX , Xebm , verbose = verbose )
 	
 	
 	## Fit distribution
@@ -298,52 +305,55 @@ if __name__ == "__main__":
 	## Multi-model
 	##============
 	clim = ns.infer_multi_model( clim , verbose = verbose )
-	
-	
-	## Keep only multi-model
-	##======================
 	climMM = clim.copy()
-	climMM.keep_models( ["multi"] )
+	climMM.keep_models( "Multi_Synthesis" )
 	
 	
 	## Apply constraints
 	##==================
-	climCX     = ns.constraints_CX( climMM , Xo , time_reference = time_reference , verbose = verbose )
-	climCXCB   = ns.constraints_bayesian( climCX , Yo , n_mcmc_drawn_min , n_mcmc_drawn_max , min_rate_accept = min_rate_accept , verbose = verbose )
-	climC0     = ns.constraints_C0( climMM , Yo , verbose = verbose )
-	climCXC0   = ns.constraints_C0( climCX , Yo , verbose = verbose )
+	climCX     = ns.constrain_covariate( climMM , Xo , time_reference , verbose = verbose )
+	climCXCB   = ns.constrain_law( climCX , Yo , n_mcmc_drawn_min , n_mcmc_drawn_max , min_rate_accept = min_rate_accept , verbose = verbose )
+	climC0     = ns.constraint_C0( climMM , Yo , verbose = verbose )
+	climCXC0   = ns.constraint_C0( climCX , Yo , verbose = verbose )
+	
 	
 	## Compute stats
 	##==============
-	clim       = ns.extremes_stats( clim       , event , verbose = verbose )
-	climCX     = ns.extremes_stats( climCX     , event , verbose = verbose )
-	climCXCB   = ns.extremes_stats( climCXCB   , event , verbose = verbose )
-	climC0     = ns.extremes_stats( climC0     , event , verbose = verbose )
-	climCXC0   = ns.extremes_stats( climCXC0   , event , verbose = verbose )
+	clim       = ns.extreme_statistics( clim     , verbose = verbose )
+	climCX     = ns.extreme_statistics( climCX   , verbose = verbose )
+	climCXCB   = ns.extreme_statistics( climCXCB , verbose = verbose )
+	climCXC0   = ns.extreme_statistics( climCXC0 , verbose = verbose )
+	
+	params     = ns.build_params_along_time( clim     , verbose = verbose )
+	paramsCX   = ns.build_params_along_time( climCX   , verbose = verbose )
+	paramsCXCB = ns.build_params_along_time( climCXCB , verbose = verbose )
+	paramsCXC0 = ns.build_params_along_time( climCXC0 , verbose = verbose )
 	
 	
 	## Save in netcdf
 	##===============
-	ns.to_netcdf( clim       , event , os.path.join( pathOut , "HW19D3_GEVMin_clim.nc"       ) , ""       )
-	ns.to_netcdf( climCX     , event , os.path.join( pathOut , "HW19D3_GEVMin_climCX.nc"     ) , "CX"     )
-	ns.to_netcdf( climCXCB   , event , os.path.join( pathOut , "HW19D3_GEVMin_climCXCB.nc"   ) , "CXCB"   )
-	ns.to_netcdf( climC0     , event , os.path.join( pathOut , "HW19D3_GEVMin_climC0.nc"     ) , "C0"     )
-	ns.to_netcdf( climCXC0   , event , os.path.join( pathOut , "HW19D3_GEVMin_climCXC0.nc"   ) , "CXC0"   )
+	for c,s in zip([clim,climCX,climCXC0,climCXCB],["","CX","CXC0","CXCB"]):
+		c.to_netcdf( os.path.join( pathOut , "{}_clim{}.nc".format(event.name,s) ) )
+	for p,s in zip([params,paramsCX,paramsCXC0,paramsCXCB],["","CX","CXC0","CXCB"]):
+		params.to_dataset( name = "params{}".format(s) ).to_netcdf( os.path.join( pathOut , "{}_params{}.nc".format(event.name,s) ) )
 	
+	## Reload
+	##=======
+#	clim,climCX,climCXC0,climCXCB = ( ns.Climatology2.from_netcdf( os.path.join( pathOut , "{}_clim{}.nc".format(event.name,s) ) , ns_law ) for s in ["","CX","CXC0","CXCB"] )
+#	params,paramsCX,paramsCXC0,paramsCXCB = ( xr.open_dataset( os.path.join( pathOut , "{}_params{}.nc".format(event.name,s) ) )["params{}".format(s)] for s in ["","CX","CXC0","CXCB"] )
 	
 	## Plot
 	##=====
-	nsp.write_package_tabular( climCXCB   , event , os.path.join( pathOut , "SummaryCXCB.txt"   ) , verbose = verbose )
-	nsp.write_package_tabular( climCXC0   , event , os.path.join( pathOut , "SummaryCXC0.txt"   ) , verbose = verbose )
-	nsp.decomposition( lX , clim.X , event                      , os.path.join( pathOut , "decomposition.pdf" ) , verbose = verbose )
-	nsp.constraints_CX( climMM , climCXCB , Xo , time_reference , os.path.join( pathOut , "constraintCX.pdf" )  , verbose = verbose )
-	nsp.plot_classic_packages( clim       , event , path = pathOut , suffix = "MM"     , ci = ci , verbose = verbose )
-	nsp.plot_classic_packages( climCX     , event , path = pathOut , suffix = "CX"     , ci = ci , verbose = verbose )
-	nsp.plot_classic_packages( climCXCB   , event , path = pathOut , suffix = "CXCB"   , ci = ci , verbose = verbose )
-	nsp.plot_classic_packages( climC0     , event , path = pathOut , suffix = "C0"     , ci = ci , verbose = verbose )
-	nsp.plot_classic_packages( climCXC0   , event , path = pathOut , suffix = "CXC0"   , ci = ci , verbose = verbose )
-	nsp.ns_params_comparison( climMM , climCXC0   , ofile = os.path.join( pathOut , "ns_paramsMM_CXC0.pdf"   ) , ci = ci , verbose = verbose )
-	nsp.ns_params_comparison( climMM , climCXCB   , ofile = os.path.join( pathOut , "ns_paramsMM_CXCB.pdf"   ) , ci = ci , verbose = verbose )
+	pltkwargs = { "verbose" : verbose , "ci" : ci }
+	nsp.GAM_decomposition( clim , lX , os.path.join( pathOut , "GAM_decomposition.pdf" ) , **pltkwargs )
+	nsp.constraint_covariate( clim , climCXCB , Xo , os.path.join( pathOut , "constraint_covariate.pdf" )  , **pltkwargs )
+	nsp.summary( clim     , pathOut , t1 = 2040 , params = params     , **pltkwargs )
+	nsp.summary( climCX   , pathOut , t1 = 2040 , params = paramsCX   , suffix = "CX"   , **pltkwargs )
+	nsp.summary( climCXC0 , pathOut , t1 = 2040 , params = paramsCXC0 , suffix = "CXC0" , **pltkwargs )
+	nsp.summary( climCXCB , pathOut , t1 = 2040 , params = paramsCXCB , suffix = "CXCB" , **pltkwargs )
+	nsp.constraint_law( climCX , climCXCB , ofile = os.path.join( pathOut , "constraint_law.pdf" ) , **pltkwargs )
+	nsp.statistics_time( [clim,climCX,climCXCB] , os.path.join( pathOut , "Statistics_time.pdf" ) , labels = clim.model.tolist() + ["Multi_CX","Multi_CXCB"] , colors = ["red","blue","green"] , **pltkwargs )
+	
 	
 	print("Done")
 
