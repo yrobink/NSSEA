2108e9ebd99bbc7f66e7309f95c8499b6c5a2764
= <yoann.robin.k@gmail.com>
2021-02-17 12:32:38 +0100

    Change folder for clean

--- clean.sh
+++ clean.sh
@@ -2,11 +2,11 @@
 
 
 ## Delete python temporary files
-rm -rf python/NSSEA.egg*
-rm -rf python/build
-rm -rf python/dist
-rm -rf python/tmp
-rm -rf python/var
+rm -rf NSSEA.egg*
+rm -rf build
+rm -rf dist
+rm -rf tmp
+rm -rf var
 
 rm -rf tests/output/Normal/*
 rm -rf tests/output/GEV/*
